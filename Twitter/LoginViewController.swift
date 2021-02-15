d7ce56ad4318f578d6b1a88e4d86e3c0920ad426
BrandonLC2020 <43575529+BrandonLC2020@users.noreply.github.com>
2021-02-14 22:34:24 -0500

    some added code

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -20,6 +20,7 @@ class LoginViewController: UIViewController {
         
         let myUrl = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: myUrl, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (<#Error#>) in
             print("Could not log in!")
